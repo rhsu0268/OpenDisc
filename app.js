fd03466bf4dd5b363d8136408ed8df4ea829339e
Richard Hsu <rhsu@gwu.edu>
2015-11-27 18:35:44 -0500

    Added code to get posts

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.config([
                 controller: 'MainCtrl'
 
             });
+
+        $stateProvider
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+
+            });
         $urlRouterProvider.otherwise('home');
 
     }
@@ -48,8 +56,15 @@ app.controller('MainCtrl', [
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
+
+
+
             $scope.title = '';
             $scope.link = '';
         }
@@ -72,3 +87,13 @@ app.factory('posts', [function() {
     return o;
 
 }]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts)
+    {
+        $scope.post = posts.posts[$stateParams.id];
+    }
+]);
