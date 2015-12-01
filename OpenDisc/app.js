770d14b4f0cecf6db30abc8f4f1a32c8dcb7217b
Richard Hsu <rhsu@gwu.edu>
2015-12-01 14:05:41 -0500

    Testing route to add posts

--- OpenDisc/app.js
+++ OpenDisc/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -14,6 +13,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/news');
 require('./models/Posts');
 require('./models/Comments');
+
+var routes = require('./routes/index');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
