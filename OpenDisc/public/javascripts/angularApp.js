0ba5d5ae509b29ad301314740155a6a661d2f868
Richard Hsu <rhsu@gwu.edu>
2015-12-15 16:59:54 -0500

    Reverted to authentication factory

--- OpenDisc/public/javascripts/angularApp.js
+++ OpenDisc/public/javascripts/angularApp.js
@@ -149,70 +149,9 @@ app.factory('posts', ['$http', function($http) {
     }
     return o;
 
-}])
-
-
-.factory('auth', ['$http', '$window', function($http, $window) {
-
-    var auth = {};
-    auth.saveToken = function(token)
-    {
-        $window.localStorage['open-disc-token'] = token;
-
-    };
-    auth.getToken = function()
-    {
-        return $window.localStorage['open-disc-token'];
-    };
-
-    auth.isLoggedIn = function()
-    {
-        var token = auth.getToken();
-
-        if (token)
-        {
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.exp > Date.now() / 1000;
-        }
-        else
-        {
-            return false;
-        }
-    };
-    auth.currentUser = function()
-    {
-        if (auth.isLoggedIn())
-        {
-            var token = auth.getToken();
-
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            return payload.username;
-        }
-    };
-
-    auth.register = function(user)
-    {
-        return $http.post('/register', user).success(function(data) {
-            auth.saveToken(data.token);
-        });
-    };
-
-    auth.logIn = function(user)
-    {
-        return $http.post('/login', user).success(function(data) {
-            auth.saveToken(data.token);
-        });
-    };
-
-    auth.logOut = function()
-    {
-        $window.localStorage.removeItem('open-disc-token');
-    };
-
-    return auth;
 
 }]);
+
 
 app.controller('PostsCtrl', [
     '$scope',
