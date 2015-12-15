6b8f4c846b1db658c6697b3d3a942a30ad01d8a2
Richard Hsu <rhsu@gwu.edu>
2015-12-14 23:39:36 -0500

    Added angular factory for authentication

--- OpenDisc/public/javascripts/angularApp.js
+++ OpenDisc/public/javascripts/angularApp.js
@@ -127,7 +127,70 @@ app.factory('posts', ['$http', function($http) {
     }
     return o;
 
-}]);
+}])
+
+
+.factory('auth', ['$sttp', '$window', function($http, $window) {
+
+    var auth = {};
+    auth.saveToken = function(token)
+    {
+        $window.localStorage(['open-disc-token']) = token;
+
+    }
+    auth.getToken = function()
+    {
+        return $window.localStorage['open-disc-token'];
+    }
+
+    auth.isLoggedIn = function()
+    {
+        var token = auth.getToken();
+
+        if (token)
+        {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp > Data.now() / 1000;
+        }
+        else
+        {
+            return false;
+        }
+
+        auth.currentUser = function()
+        {
+            if (auth.isLoggedIn)
+            {
+                var token = auth.getToken();
+
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+                return payload.username;
+            }
+        }
+
+        auth.register = function(user)
+        {
+            return $http.post('/register', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        }
+
+        auth.login = function(user)
+        {
+            return $http.post('/login', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        }
+
+        auth.logout = function()
+        {
+            $window.localStorage.removeItem('open-disc-token');
+        }
+    }
+    return auth;
+
+}])
 
 app.controller('PostsCtrl', [
     '$scope',
--- OpenDisc/routes/index.js
+++ OpenDisc/routes/index.js
@@ -70,8 +70,9 @@ router.get('/posts', function(req, res, next) {
 
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function(err, post) {
         if (err)
@@ -135,7 +136,7 @@ router.get('/posts/:post', function(req, res) {
     });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
     req.post.upvote(function(err, post) {
         if (err)
         {
@@ -146,7 +147,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
     req.comment.upvote(function(err, comment) {
         if (err)
         {
@@ -157,9 +158,10 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
     });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment) {
         if (err)
